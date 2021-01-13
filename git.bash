bd56a2c922ee60ba9463f520b7bb0fa37446fc21
Taylor Andrew Hale <taylorhale@taylors-air.ddns.uark.edu>
2021-01-13 15:15:32 -0600

    First Commit

---
+++ git.bash
@@ -0,0 +1,10 @@
+
+echo "# FirstRepository" >> README.md
+git add README.md
+
+git init
+git remote add origin https://github.com/Anjin-Byte/FirstRepository.git
+git add .
+git status
+git commit -m "First Commit"
+git push -u origin master
